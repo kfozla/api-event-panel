9be7092bc08e27eb5d5888c8663c4097571220f7
Yusuf Gökdağ <yusufgokdag0726@gmail.com>
2025-07-29 15:51:41 +0300

    dashboard integration

--- obj/Debug/net8.0/api-event-panel.AssemblyInfo.cs
+++ obj/Debug/net8.0/api-event-panel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api-event-panel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad5ebd9c3cdc08132d92dc8e999e6908d57e456c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2284ce7c986f64b21046609ec71b64758306257e")]
 [assembly: System.Reflection.AssemblyProductAttribute("api-event-panel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api-event-panel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
